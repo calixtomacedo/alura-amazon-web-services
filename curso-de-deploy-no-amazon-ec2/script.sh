50934c9433e06d43e8b088b2d684fbb57c46d0f7
Calixto Macedo <calixto.macedo@gmail.com>
2021-10-02 07:51:37 -0300

    03.Automatizando a instancias EC2

--- curso-de-deploy-no-amazon-ec2/script.sh
+++ curso-de-deploy-no-amazon-ec2/script.sh
@@ -1,18 +1,19 @@
-
-
 #!/bin/bash
+#Atualizando os pacotes
 yum update -y
+
+#Configurando os repositórios
 amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+
+#Instalando o apache e o mysql
 yum install -y httpd mariadb-server
+
+#inicialização automática
 systemctl start httpd
 systemctl enable httpd
 systemctl start mariadb
 systemctl enable mariadb
+
+#Ajustando o permissionamento
 usermod -a -G apache ec2-user
 chown -R ec2-user:apache /var/www
-
-ssh -i "aws-calixto.pem" ec2-user@ec2-54-89-173-89.compute-1.amazonaws.com
-
-
-Status em portas
-netstat -ltun 
